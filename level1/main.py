18011472aecf32906d1bd98391539405b636f4a1
Alice Caron <alicecaron94@gmail.com>
2019-02-13 22:35:49 +0100

    Level 1

--- level1/main.py
+++ level1/main.py
@@ -1,3 +1,16 @@
 import json
 
-# your code
+json_data=open('data.json').read()
+data = json.loads(json_data)
+
+workers = []
+for user in data["workers"]:
+    userId = user["id"]
+    userShifts = filter(lambda shift: shift["user_id"] == userId, data["shifts"])
+    userPrice = len(userShifts) * user["price_per_shift"]
+    workers.append({'id':userId, 'price':userPrice})
+
+output = {'workers': workers}
+
+with open('output.json', 'w') as outfile:
+    json.dump(output, outfile)
